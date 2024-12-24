c97b1460f980171eb3c5355f59d8a63c8d3ef443
salanderm <142754240+salanderm@users.noreply.github.com>
2024-12-24 14:26:40 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,16 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+# Borough and location handling
+st.header("Select Location")
+borough = st.selectbox("Select a Borough", ["Bronx", "Manhattan", "Brooklyn", "Queens", "Staten Island","Unknown"])
+transaction_month= st.text_input("Enter the month as int")
+
+"""
+'transaction_month', 'transaction_day',
+       'transaction_hour', 'transaction_week_day', 'weekend', 'is_holiday', 'Borough']
+input_features = categorical_features + ['temp', 'humidity', 'windspeed', 'cloudcover',
+       'precip']
+"""
