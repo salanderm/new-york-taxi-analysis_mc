07832fbde5426e5d12126e52e2bd03d5c296bc6e
salanderm <142754240+salanderm@users.noreply.github.com>
2024-12-24 15:06:11 +0000

    third commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,81 @@
 import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Session state initialization
+if "prediction_done" not in st.session_state:
+    st.session_state.prediction_done = False
+if "predicted_amt" not in st.session_state:
+    st.session_state.predicted_amt = None
 
+def reset_app():
+    st.session_state.prediction_done = False
+    st.session_state.predicted_amt = None
 
-# Borough and location handling
-st.header("Select Location")
-borough = st.selectbox("Select a Borough", ["Bronx", "Manhattan", "Brooklyn", "Queens", "Staten Island","Unknown"])
-transaction_month= st.selectbox("Enter the month", [1,2,3,4,5,6,7,9,10,11,12])
-transaction_day = st.selectbox("Enter the day", [1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31])
-transaction_hour = st.selectbox("Enter the hour", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
-transaction_week_day = st.selectbox("Enter the weekday", [0,1,2,3,4,5,6])
-weekend = st.selectbox("Is it weekend?", [0,1])
-is_holiday = st.selectbox("Is it holiday?", [0,1])
-temp = st.number_input("Enter the temperature")
-humidity = st.number_input("Enter the humidity")
-windspeed = st.number_input("Enter the windspeed")
-cloudcover = st.number_input("Enter the amount of cloudcover")
-precip = st.number_input("Enter the precipitation amount")
-"""
-'transaction_month', 'transaction_day',
-       'transaction_hour', 'transaction_week_day', 'weekend', 'is_holiday', 'Borough']
-input_features = categorical_features + ['temp', 'humidity', 'windspeed', 'cloudcover',
-       'precip']
-"""
+# Main app
+if not st.session_state.prediction_done:
+    st.title("🎈 My new app")
+    st.write(
+        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    )
+
+    # Borough and location handling
+    st.header("Select Location")
+    borough = st.selectbox("Select a Borough", ["Bronx", "Manhattan", "Brooklyn", "Queens", "Staten Island", "Unknown"])
+    transaction_month = st.selectbox("Enter the month", list(range(1, 13)))
+    transaction_day = st.selectbox("Enter the day", list(range(1, 32)))
+    transaction_hour = st.selectbox("Enter the hour", list(range(24)))
+    transaction_week_day = st.selectbox("Enter the weekday", list(range(7)))
+    weekend = st.selectbox("Is it weekend?", [0, 1])
+    is_holiday = st.selectbox("Is it holiday?", [0, 1])
+    temp = st.number_input("Enter the temperature")
+    humidity = st.number_input("Enter the humidity")
+    windspeed = st.number_input("Enter the windspeed")
+    cloudcover = st.number_input("Enter the amount of cloudcover")
+    precip = st.number_input("Enter the precipitation amount")
+
+    # Load column holders and model
+    X_test = pd.read_csv("dummy_column_holder.csv")
+    nyc_class = pd.read_csv("nyc_class.csv")
+
+    pre_input_array = [transaction_month, transaction_day, transaction_hour, transaction_week_day, weekend, is_holiday, temp, humidity, windspeed, cloudcover, precip]
+
+    def get_location(loc):
+        int_csv = pd.read_csv("data/taxi_zone_lookup.csv")[['LocationID', 'Borough']]
+        return int_csv[int_csv["Borough"] == loc].values[0][0]
+
+    def get_location_array(loc_id, column_names):
+        new_list = list(np.zeros(len(column_names)))
+        loc_index = column_names.index(f'PULocationID_{loc_id}')
+        new_list[loc_index] = 1.0
+        return list(map(float, new_list))
+
+    def get_borough_array(borough):
+        borough_lookup = list(nyc_class["Borough"].value_counts().keys())
+        b_count = len(borough_lookup)
+        b_count_list = list(map(float, list(np.zeros(b_count))))
+        b_count_list[borough_lookup.index(borough)] = 1.0
+        return b_count_list
+
+    column_names = [x for x in X_test.columns if x.startswith('PULocationID')]
+    loc_id = get_location(borough)
+    loc_array = get_location_array(loc_id, column_names)
+    borough_array = get_borough_array(borough)
+
+    # Load the model
+    with open("model_at_hand.pkl", "rb") as f:
+        model_at_hand = pickle.load(f)
+
+    input_array = pre_input_array + loc_array + borough_array
+    input_array = np.array(input_array).reshape(1, -1)
+
+    if st.button("Predict"):
+        predicted_amt = model_at_hand.predict(input_array)[0]
+        st.session_state.prediction_done = True
+        st.session_state.predicted_amt = predicted_amt
+
+else:
+    st.header(f"💵 PREDICTED AMOUNT: ${st.session_state.predicted_amt:.2f}")
+    if st.button("🔙 Back"):
+        reset_app()
